3834a7ecfc9cc5902d0f3c57b2c2cece5aa91576
Andry925 <andry925@ukr.net>
2024-10-29 12:19:47 +0200

    feat/implement-first-part

---
+++ Laba4/js/script.js
@@ -0,0 +1,5 @@
+const firstElementColorChange = () =>
+  document.getElementById('ninth-element').classList.toggle('invert');
+
+const secondElementColorChange = () =>
+  document.querySelector('#tenth-element').classList.toggle('invert');
